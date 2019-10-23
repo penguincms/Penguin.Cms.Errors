ed9be690543cb1de0f754c94199430935687d4c3
anloffredo <loffredo.ap@gmail.com>
2019-10-23 00:55:11 -0700

    Framework extraction update
    Framework extraction update

---
+++ AuditableError.cs
@@ -0,0 +1,125 @@
+﻿using Penguin.Auditing.Abstractions.Attributes;
+using Penguin.Cms.Entities;
+using Penguin.Extensions.Exceptions;
+using Penguin.Persistence.Abstractions.Attributes.Control;
+using Penguin.Persistence.Abstractions.Attributes.Validation;
+using System;
+using System.Diagnostics.Contracts;
+
+namespace Penguin.Cms.Errors
+{
+    /// <summary>
+    /// Represents an auditable error to be persisted
+    /// </summary>
+    [DontAuditChanges]
+    public class AuditableError : Entity
+    {
+        /// <summary>
+        /// The generated int Id for the error provided by the environment
+        /// </summary>
+        [DontAllow(DisplayContexts.List | DisplayContexts.Edit)]
+        public long ErrorId { get; set; }
+
+        /// <summary>
+        /// The message provided with the error
+        /// </summary>
+
+        [DontAllow(DisplayContexts.Edit | DisplayContexts.BatchEdit)]
+        public string Exception { get; set; }
+
+        /// <summary>
+        /// The full name of the type of error that was thrown
+        /// </summary>
+
+        [DontAllow(DisplayContexts.List | DisplayContexts.Edit)]
+        public string ExceptionType { get; set; }
+
+        /// <summary>
+        /// Any form values that were posted if this was a web request.
+        /// </summary>
+
+        [DontAllow(DisplayContexts.List | DisplayContexts.Edit)]
+        public string FormValues { get; set; }
+
+        /// <summary>
+        /// The top level inner exception message
+        /// </summary>
+
+        [DontAllow(DisplayContexts.Edit | DisplayContexts.BatchEdit)]
+        public string InnerException { get; set; }
+
+        /// <summary>
+        /// If a web exception, this is the referrer for the request
+        /// </summary>
+
+        [DontAllow(DisplayContexts.List | DisplayContexts.Edit)]
+        public string RawReferrer { get; set; }
+
+        /// <summary>
+        /// If a web exception, this is the Url that was requested
+        /// </summary>
+
+        [DontAllow(DisplayContexts.List | DisplayContexts.Edit)]
+        public string RawRequestUrl { get; set; }
+
+        /// <summary>
+        /// If a web exception, this is the referrer for the request
+        /// </summary>
+
+        [DontAllow(DisplayContexts.List | DisplayContexts.Edit)]
+        public string Referrer { get; set; }
+
+        /// <summary>
+        /// If a web exception, this is the Url that was requested
+        /// </summary>
+
+        [DontAllow(DisplayContexts.Edit | DisplayContexts.BatchEdit)]
+        public string RequestUrl { get; set; }
+
+        /// <summary>
+        /// The stack trace provided by the environment
+        /// </summary>
+        [MaxLength]
+        [DontAllow(DisplayContexts.List | DisplayContexts.Edit)]
+        public string StackTrace { get; set; }
+
+        /// <summary>
+        /// An ID representing the user that caused the error, or 0 if unknown
+        /// </summary>
+
+        [DontAllow(DisplayContexts.List | DisplayContexts.Edit)]
+        public Guid UserId { get; set; }
+
+        /// <summary>
+        /// Creates a new instance of this error class
+        /// </summary>
+        public AuditableError()
+        {
+            this.UserId = Guid.Empty;
+            this.DateCreated = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Creates a new instance of this error class, and then fills it with information
+        /// </summary>
+        /// <param name="ex">The exception to use when filling this instance</param>
+        public AuditableError(Exception ex)
+        {
+            Contract.Requires(ex != null);
+
+            this.UserId = Guid.Empty;
+            this.DateCreated = DateTime.Now;
+            this.StackTrace = string.Empty;
+            Type exceptionType = ex.GetType();
+            this.ExceptionType = exceptionType.ToString();
+
+            if (ex.InnerException != null)
+            {
+                this.InnerException = ex.InnerException.RecursiveMessage();
+            }
+
+            this.Exception = ex.RecursiveMessage();
+            this.StackTrace += ex.RecursiveStackTrace();
+        }
+    }
+}
---
+++ Extensions/AuditableErrorRepositoryExtensions.cs
@@ -0,0 +1,87 @@
+﻿using Penguin.Persistence.Abstractions.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Penguin.Cms.Errors.Extensions
+{
+    public static class AuditableErrorRepositoryExtensions
+    {
+        /// <summary>
+        /// Adds a new exception to the underlying storage context
+        /// </summary>
+        /// <param name="errorRepository"></param>
+        /// <param name="ex">The exception to add</param>
+        /// <param name="url">If relevant, any URL associated with the error</param>
+        /// <param name="UserId">If relevant, the ID of the user that generated the error</param>
+        public static void Add(this IRepository<AuditableError> errorRepository, Exception ex, string url = null, Guid? UserId = null)
+        {
+            if (errorRepository is null)
+            {
+                throw new ArgumentNullException(nameof(errorRepository));
+            }
+
+            AuditableError thisError = new AuditableError(ex)
+            {
+                RequestUrl = url,
+                UserId = UserId ?? Guid.Empty
+            };
+
+            errorRepository.Add(thisError);
+        }
+
+        /// <summary>
+        /// Opens a new writecontext, attempts to add an error, and closes the write context.
+        /// If the context was already open, it may not persist until it closes.
+        /// Can be called on a null repository. This will be treated as an error and caught/rethrown as
+        /// instructed by the "rethrow" parameter
+        /// </summary>
+        /// <param name="errorRepository">The Error repository to attempt to use as a source</param>
+        /// <param name="ex">The exception</param>
+        /// <param name="rethrow">If theres an error writing the error, should the error be rethrown?</param>
+        /// <param name="url">If relevant, any URL associated with the error</param>
+        /// <param name="UserId">If relevant, the ID of the user that generated the error</param>
+        /// <returns>False if an error occurs, regardless of whether or not it was rethrown</returns>
+        public static bool TryAdd(this IRepository<AuditableError> errorRepository, Exception ex, bool rethrow = false, string url = null, Guid? UserId = null)
+        {
+            try
+            {
+                using (IWriteContext context = errorRepository?.WriteContext())
+                {
+                    AuditableError thisError = new AuditableError(ex)
+                    {
+                        RequestUrl = url,
+                        UserId = UserId ?? Guid.Empty
+                    };
+
+                    errorRepository.Add(thisError);
+                }
+
+                return true;
+            }
+            catch (Exception)
+            {
+                if (rethrow)
+                {
+                    throw;
+                }
+
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Opens a new writecontext, attempts to add an error, and closes the write context.
+        /// If the context was already open, it may not persist until it closes.
+        /// Can be called on a null repository. This will be treated as an error and caught/rethrown as
+        /// instructed by the "rethrow" parameter
+        /// </summary>
+        /// <param name="errorRepository">The Error repository to attempt to use as a source</param>
+        /// <param name="error">The exception</param>
+        /// <param name="rethrow">If theres an error writing the error, should the error be rethrown?</param>
+        /// <param name="url">If relevant, any URL associated with the error</param>
+        /// <param name="UserId">If relevant, the ID of the user that generated the error</param>
+        /// <returns>False if an error occurs, regardless of whether or not it was rethrown</returns>
+        public static bool TryAdd(this IRepository<AuditableError> errorRepository, string error, bool rethrow = false, string url = null, Guid? UserId = null) => errorRepository.TryAdd(new Exception(error), rethrow, url, UserId);
+    }
+}
